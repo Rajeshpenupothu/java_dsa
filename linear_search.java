d568bacadfab656f0c87b51166c35e70b3354b16
rajeshpenupothu <rajeshpenupothu@gmail.com>
2024-07-23 22:01:36 +0530

    first commit

---
+++ linear_search.java
@@ -0,0 +1,20 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int nums[]={23,34,45,56};
+        int target=56;
+       
+        int result=linearsearch(nums,target);
+        if(result != -1)
+        System.out.println("Element fount at index:"+result);
+        else
+        System.out.println("Element not fount");
+        }
+        public static int linearsearch(int[] nums,int target){
+            for(int i=0;i<nums.length;i++)
+           
+            if(nums[i]==target)
+            return i;
+          
+            return -1;
+        }
+}
