6156ab6808ff39ba69aa61c50dd8b5a8ac042143
rajeshpenupothu <rajeshpenupothu@gmail.com>
2024-09-11 22:10:35 +0530

    insertion sort

--- insertion_sort.java
+++ insertion_sort.java
@@ -7,9 +7,9 @@ public class insertion_sort{
         {
             int j=i-1;//and the outer loop j .... the value should be i-1
             int key=arr[i];
-            while(j>= 0 && arr[j]<key){//while loop is used to satisfiy the conditions given
+            while(j>= 0 && arr[j]>key){//while loop is used to satisfiy the conditions given
                 arr[j+1]=arr[j];//if the conditions satisfy then the next value of arr[j+1] becomes arr[j] 
-                j--;
+                j--; // if the above line satisfies then this line executes and the j value shoud be j-- or j-1
             }
             arr[j+1]=key;
         }
