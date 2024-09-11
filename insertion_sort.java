f6c173432a03bf525f2100f3f764a3c922f16be2
rajeshpenupothu <rajeshpenupothu@gmail.com>
2024-09-11 21:33:19 +0530

    insertion sort

--- insertion_sort.java
+++ insertion_sort.java
@@ -1,14 +1,14 @@
 public class insertion_sort{
     public static void main(String a[]){
         
-        int arr[]={2,1,8,5};
+        int arr[]={2,1,8,5};//having a array 
        
-        for(int i=0;i<arr.length;i++)
+        for(int i=0;i<arr.length;i++)//i is the inner loop where it can starts from index value with 0 
         {
-            int j=i-1;
+            int j=i-1;//and the outer loop j .... the value should be i-1
             int key=arr[i];
-            while(j>= 0 && arr[j]<key){
-                arr[j+1]=arr[j];
+            while(j>= 0 && arr[j]<key){//while loop is used to satisfiy the conditions given
+                arr[j+1]=arr[j];//if the conditions satisfy then the next value of arr[j+1] becomes arr[j] 
                 j--;
             }
             arr[j+1]=key;
