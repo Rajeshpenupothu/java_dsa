0a66e9d6eb60666be1f2d29ead6e30d93b491daa
rajeshpenupothu <rajeshpenupothu@gmail.com>
2024-09-12 23:49:22 +0530

    quick_sort

---
+++ quick_sort.java
@@ -0,0 +1,11 @@
+public class quick_sort{
+    public void quicksort(in[] arr,int low,int high)
+    public static void main(String[] args) {
+        int arr[]={4,8,2,9,5};
+        quicksort(arr,0,arr.length)
+        for(int num:arr)
+        {
+            System.out.println(num + " ");
+        }
+    }
+}
